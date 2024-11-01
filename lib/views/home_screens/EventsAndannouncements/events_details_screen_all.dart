88ce51c4f8c6b4af936c7dbf80e3dbc4c9d9c590
Gul-Is-Here <gulfarazahmed58@gmail.com>
2024-11-01 16:25:00 -0500

    'Redesign dayn 3'

--- lib/views/home_screens/EventsAndannouncements/events_details_screen_all.dart
+++ lib/views/home_screens/EventsAndannouncements/events_details_screen_all.dart
@@ -1,63 +1,55 @@
-import 'package:community_islamic_app/constants/color.dart';
-import 'package:community_islamic_app/controllers/home_events_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
+import 'package:community_islamic_app/constants/color.dart';
+import 'package:community_islamic_app/controllers/home_events_controller.dart';
+import 'package:community_islamic_app/model/home_events_model.dart';
 
 class AllEventsDatesScreen extends StatelessWidget {
-  AllEventsDatesScreen({super.key});
   final HomeEventsController eventsController = Get.put(HomeEventsController());
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          leading: IconButton(
-              onPressed: () {
-                Get.back();
-              },
-              icon: Icon(
-                Icons.arrow_back,
-                color: whiteColor,
-              )),
-          title: Text(
-            'All Events Calendar',
-            style: TextStyle(color: whiteColor, fontFamily: popinsMedium),
-          ),
-          backgroundColor: primaryColor),
+        leading: IconButton(
+          onPressed: () => Get.back(),
+          icon: Icon(Icons.arrow_back, color: whiteColor),
+        ),
+        title: Text(
+          'All Events Calendar',
+          style: TextStyle(color: whiteColor, fontFamily: popinsMedium),
+        ),
+        backgroundColor: primaryColor,
+      ),
       body: Obx(() {
-        // Check if the events are still loading
         if (eventsController.isLoading.value) {
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        }
-        // Check if there are no events
-        else if (eventsController.events.value == null ||
+          return const Center(child: CircularProgressIndicator());
+        } else if (eventsController.events.value == null ||
             eventsController.events.value!.data.events.isEmpty) {
-          return const Center(
-            child: Text('No Events Found'),
-          );
-        }
-        // If events are loaded, pass them to the calendar
-        else {
-          // Extract the event dates as a Map<DateTime, String>
-          Map<DateTime, String> eventDates = {};
+          return const Center(child: Text('No Events Found'));
+        } else {
+          Map<DateTime, List<Event>> eventDates = {};
+
           for (var event in eventsController.events.value!.data.events) {
-            // Convert event date string to DateTime and ignore the time part
-            DateTime eventDate =
-                DateTime.parse(event.eventDate.toIso8601String());
-            DateTime eventDay =
-                DateTime(eventDate.year, eventDate.month, eventDate.day);
-            eventDates[eventDay] = event.eventDetail;
-            print('Events Dates ${eventDates[eventDay]}');
+            DateTime eventDay = DateTime(
+              event.eventDate.year,
+              event.eventDate.month,
+              event.eventDate.day,
+            );
+
+            if (eventDates.containsKey(eventDay)) {
+              eventDates[eventDay]!.add(event);
+            } else {
+              eventDates[eventDay] = [event];
+            }
           }
 
           return Padding(
             padding: const EdgeInsets.all(16.0),
             child: CalendarWidget(
-              initialDate: DateTime.now(), // Initial month to show
-              eventDates: eventDates, // Pass the event dates
+              initialDate: DateTime.now(),
+              eventDates: eventDates,
             ),
           );
         }
@@ -68,13 +60,13 @@ class AllEventsDatesScreen extends StatelessWidget {
 
 class CalendarWidget extends StatefulWidget {
   final DateTime initialDate;
-  final Map<DateTime, String> eventDates;
+  final Map<DateTime, List<Event>> eventDates;
 
   const CalendarWidget({
-    super.key,
+    Key? key,
     required this.initialDate,
     required this.eventDates,
-  });
+  }) : super(key: key);
 
   @override
   _CalendarWidgetState createState() => _CalendarWidgetState();
@@ -82,34 +74,50 @@ class CalendarWidget extends StatefulWidget {
 
 class _CalendarWidgetState extends State<CalendarWidget> {
   late DateTime _currentDate;
+  DateTime? _selectedDate;
+  List<Event>? _selectedEvents;
 
   @override
   void initState() {
     super.initState();
-    // Set the initial date as the current date or the date of the first event if applicable
     _currentDate = widget.initialDate;
+    _initializeSelectedDateAndEvents();
+  }
+
+  void _initializeSelectedDateAndEvents() {
+    // Check if today has events, otherwise use the first date with events
+    if (widget.eventDates.containsKey(_currentDate)) {
+      _selectedDate = _currentDate;
+    } else {
+      _selectedDate = widget.eventDates.keys.first;
+    }
+    _updateSelectedEvents(_selectedDate!);
+  }
+
+  void _updateSelectedEvents(DateTime date) {
+    setState(() {
+      _selectedDate = date;
+      _selectedEvents = widget.eventDates[date] ?? [];
+    });
   }
 
   @override
   Widget build(BuildContext context) {
+    final HomeEventsController eventsController =
+        Get.put(HomeEventsController());
     int daysInMonth =
         DateTime(_currentDate.year, _currentDate.month + 1, 0).day;
 
     return Column(
       children: [
-        // Calendar Header with Previous/Next buttons
+        // Month and Year Navigation
         Padding(
           padding: const EdgeInsets.symmetric(vertical: 8.0),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               IconButton(
-                onPressed: () {
-                  setState(() {
-                    _currentDate =
-                        DateTime(_currentDate.year, _currentDate.month - 1);
-                  });
-                },
+                onPressed: () => _changeMonth(-1),
                 icon: const Icon(Icons.arrow_back_ios),
                 color: primaryColor,
               ),
@@ -118,44 +126,80 @@ class _CalendarWidgetState extends State<CalendarWidget> {
                 style: const TextStyle(fontSize: 20, fontFamily: popinsMedium),
               ),
               IconButton(
-                onPressed: () {
-                  setState(() {
-                    _currentDate =
-                        DateTime(_currentDate.year, _currentDate.month + 1);
-                  });
-                },
+                onPressed: () => _changeMonth(1),
                 icon: const Icon(Icons.arrow_forward_ios),
                 color: primaryColor,
               ),
             ],
           ),
         ),
-        // Calendar Days
+        // Calendar Display
         Table(
-          children: _generateCalendar(
-              daysInMonth, widget.eventDates, _currentDate, context),
+          children: _generateCalendar(daysInMonth),
         ),
+        const SizedBox(height: 16.0),
+        // Events List for Selected Date
+        if (_selectedEvents != null && _selectedEvents!.isNotEmpty)
+          Expanded(
+            child: ListView.builder(
+              itemCount: _selectedEvents!.length,
+              itemBuilder: (context, index) {
+                final event = _selectedEvents![index];
+                final feedImage = eventsController.feedsList.length > index
+                    ? eventsController.feedsList[index].feedImage
+                    : null;
+
+                return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: Card(
+                    child: Column(
+                      children: [
+                        ListTile(
+                          title: Text(
+                            event.eventDetail,
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: primaryColor,
+                              fontFamily: popinsMedium,
+                            ),
+                          ),
+                          subtitle: Text(
+                            DateFormat('MMMM d, yyyy h:mm a')
+                                .format(event.eventDate),
+                          ),
+                        ),
+                        if (feedImage != null)
+                          Image.network(
+                            feedImage,
+                            fit: BoxFit.cover,
+                            height: 150,
+                            width: double.infinity,
+                          ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          )
+        else
+          const Center(child: Text('No Events for Selected Date')),
       ],
     );
   }
 
-  // Helper function to check if two dates (without time) are the same
-  bool _isSameDate(DateTime date1, DateTime date2) {
-    return date1.year == date2.year &&
-        date1.month == date2.month &&
-        date1.day == date2.day;
+  void _changeMonth(int delta) {
+    setState(() {
+      _currentDate = DateTime(_currentDate.year, _currentDate.month + delta);
+    });
+    _updateSelectedEvents(DateTime(_currentDate.year, _currentDate.month, 1));
   }
 
-  // Generates the rows for the calendar
-  List<TableRow> _generateCalendar(
-      int daysInMonth,
-      Map<DateTime, String> eventDates,
-      DateTime currentDate,
-      BuildContext context) {
+  List<TableRow> _generateCalendar(int daysInMonth) {
     List<TableRow> rows = [];
     List<Widget> dayCells = [];
 
-    // Add day names (e.g., Mon, Tue, Wed...)
+    // Weekday Headers
     dayCells.addAll(['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
         .map((day) => Center(
               child: Text(
@@ -167,87 +211,52 @@ class _CalendarWidgetState extends State<CalendarWidget> {
         .toList());
     rows.add(TableRow(children: dayCells));
 
-    // Fill initial empty cells if the first day of the month doesn't start on Monday
+    // Calendar Days
     dayCells = [];
-    int firstWeekday = DateTime(currentDate.year, currentDate.month, 1).weekday;
+    int firstWeekday =
+        DateTime(_currentDate.year, _currentDate.month, 1).weekday;
+
+    // Add Empty Cells for Alignment
     for (int i = 1; i < firstWeekday; i++) {
       dayCells.add(const SizedBox());
     }
 
-    // Generate day numbers
+    // Days of the Month
     for (int day = 1; day <= daysInMonth; day++) {
-      DateTime date = DateTime(currentDate.year, currentDate.month, day);
-      bool isEventDay =
-          eventDates.keys.any((eventDate) => _isSameDate(date, eventDate));
+      DateTime date = DateTime(_currentDate.year, _currentDate.month, day);
+      bool isEventDay = widget.eventDates.containsKey(date);
+      bool isSelected = date == _selectedDate;
 
-      dayCells.add(Center(
-        child: Tooltip(
-          message: isEventDay
-              ? eventDates[date]!
-              : "", // Show event detail on tooltip
-          child: GestureDetector(
-            onTap: () {
-              if (isEventDay) {
-                // Show event detail dialog or any other action
-                showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return AlertDialog(
-                      title: Text(
-                        'Event Details',
-                        style: TextStyle(
-                            color: primaryColor, fontFamily: popinsSemiBold),
-                      ),
-                      content: Text(
-                        eventDates[date]!,
-                        style: TextStyle(fontFamily: popinsMedium),
-                      ),
-                      actions: <Widget>[
-                        TextButton(
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                          child: Text(
-                            'Close',
-                            style: TextStyle(
-                                color: primaryColor,
-                                fontFamily: popinsSemiBold),
-                          ),
-                        ),
-                      ],
-                    );
-                  },
-                );
-              }
-            },
-            child: Container(
-              padding: const EdgeInsets.all(8.0),
-              decoration: BoxDecoration(
-                color: isEventDay
+      dayCells.add(GestureDetector(
+        onTap: () {
+          _updateSelectedEvents(date);
+        },
+        child: Container(
+          padding: const EdgeInsets.all(8.0),
+          decoration: BoxDecoration(
+            color: isSelected
+                ? Colors.blue
+                : isEventDay
                     ? Colors.red
-                    : Colors.transparent, // Highlight event day with red
-                shape: BoxShape.circle,
-              ),
-              child: Text(
-                day.toString(),
-                style: TextStyle(
-                  color: isEventDay ? Colors.white : Colors.black87,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
+                    : Colors.transparent,
+            shape: BoxShape.circle,
+          ),
+          child: Text(
+            day.toString(),
+            style: TextStyle(
+              color: isSelected || isEventDay ? Colors.white : Colors.black87,
+              fontWeight: FontWeight.bold,
             ),
           ),
         ),
       ));
 
-      // Add row every Sunday (7th day of the week)
       if ((day + firstWeekday - 1) % 7 == 0) {
         rows.add(TableRow(children: dayCells));
         dayCells = [];
       }
     }
 
-    // Add any remaining day cells
     if (dayCells.isNotEmpty) {
       while (dayCells.length < 7) {
         dayCells.add(const SizedBox());
