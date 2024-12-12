22199ee7ddcb11b97922539d0154ac62d3d2a947
Gul-Is-Here <gulfarazahmed58@gmail.com>
2024-12-12 16:24:10 -0600

    'Day 23 '

--- lib/constants/color.dart
+++ lib/constants/color.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-Color primaryColor = Color(0xFF003231);
-Color containerConlor = Color(0xFF061F32);
-Color asColor = Color(0xFFEDAE33);
+Color primaryColor = const Color(0xFF003231);
+Color containerConlor = const Color(0xFF061F32);
+Color asColor = const Color(0xFFEDAE33);
 const buttonColorP = Color(0XFF123443);
 Color whiteColor = Colors.white;
 
@@ -10,6 +10,7 @@ Color goldenColor = const Color(0xFFFDB408);
 Color goldenColor2 = const Color(0xFF956100);
 // Color goldenColor23 = Color.fromARGB(255, 176, 137, 64);
 Color lightColor = const Color(0xFFC4F1DD);
+Color secondaryColor = Color(0xFF00A53C);
 
 /// Arabic Fonts
 const jameelNori1 = 'JameelNori1';
